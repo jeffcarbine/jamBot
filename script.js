5f70c942cb6c8f61558403f2c670867059f6eba2
jeffcarbine <jeffcarbine@gmail.com>
2014-10-21 15:35:45 -0600

    Started work on script.js
    Began building the basic functions that will make script.js run on
    jam.html

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // JavaScript Document
+
+var button = document.getElementById('button');
+
+startJam = function()
+{
+	console.log('startJam started');
+	
+	var timeSignatures = document.getElementById('timeSignatures');
+	var bpm = document.getElementById('bpm');
+	console.log('1');
+	console.log(timeSignatures);
+	console.log(bpm);
+	console.log('2');
+	
+	if((timeSignatures == '2/2') && (bpm == '100')) 
+	{
+		console.log('I am working!');	
+	}
+}
+		
+button.onClick = startJam();
