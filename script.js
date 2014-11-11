4162b3aa125f746be3c9a7230a4cd971a4191edf
jeffcarbine <jeffcarbine@gmail.com>
2014-11-11 15:46:58 -0700

    Metronome works!
    Metronome now converts BPM to milliseconds and counts up from 0 to 3.
    Changed jam.html to have inputs rather than selects.

--- script.js
+++ script.js
@@ -1,28 +1,62 @@
-// JavaScript Document
+// Creating the two variables needed
+// for the metronome to be decoded
+var bpmString = 0;
+var bpm = 60;
+var bpb = 4;
+i = 0;
 
-var frequencyString = 0;
-var frequencyParsed = 0;
-
-function runLoop()
+// This function starts the metronome
+function initLoop()
 {
-	if(frequencyParsed > 0) clearInterval(frequencyParsed);	
-	console.log(frequencyParsed);
+	// Clear metronome when restarting
+	if(bpm > 0) clearInterval(bpm);	
+	if(bpb > 0) clearInterval(bpb);
 	
-	var frequencyString = document.getElementById('frequencyString').value;
-	console.log('frequencyString is ' + frequencyString);
+	// Get the metronome number from the drop-down
+	// menu selection
+	var bpmString = document.getElementById('bpm').value;
+	var bpbString = document.getElementById('bpb').value;
 	
-	var frequencyParsed = parseInt(frequencyString);
-	console.log(frequencyParsed);
+	// Change the metronome string to number
+	var bpm = parseInt(bpmString);
+	var bpb = parseInt(bpbString);
 	
-	metronome = setInterval('startLoop()', frequencyParsed);	
-	
-	console.log(frequencyParsed);
+	// Start the metronome - uses 
+	// function startLoop() below	
+	metronome = setInterval('startLoop()', (60000/bpm));
+
 }
 
+// This tells the metronome what
+// to do on each marked or non-marked beat
+function checkBeat() {
+	if (i == 0) {
+		console.log('Marked beat');
+	} else {
+		console.log('Non-marked beat');	
+	}
+}	
+
+// This loops through the metronome, restarting
+// after the beats reach the end of the measure
 function startLoop() {
-	console.log('Loop has initiated');
+	// i is the number of beats per measure
+	// remember!! it starts with zero, so it is always
+	// one less than the beats per measure
+	if(i <= (bpb - 1)) {
+		console.log(i)
+		checkBeat();
+		i++;	
+	} else {
+		i = 0;
+		console.log(i);
+		checkBeat();
+		i++;
+	}
+
 }
 
+// This starts the metronome loop
 function stopLoop() {
 	clearInterval(metronome);	
 }
